02b9198574308ef1069a617964c5d586f7f57d40
cineris <cineris1104@gmail.com>
2014-11-12 21:19:13 -0500

    Fixed jshint issues with polyfill.

--- web/src/main/webapp/app/scripts/app.js
+++ web/src/main/webapp/app/scripts/app.js
@@ -12,7 +12,7 @@ angular.module("app", ['ngRoute', 'app.home', 'app.shared'])
         if (!Array.prototype.indexOf) {
             Array.prototype.indexOf = function(searchElement, fromIndex) {
                 var k;
-                if (this == null) {
+                if (this === null) {
                     throw new TypeError('"this" is null or not defined');
                 }
                 var O = Object(this);
@@ -29,7 +29,6 @@ angular.module("app", ['ngRoute', 'app.home', 'app.shared'])
                 }
                 k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
                 while (k < len) {
-                    var kValue;
                     if (k in O && O[k] === searchElement) {
                         return k;
                     }
