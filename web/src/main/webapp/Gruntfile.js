a2e3141ab99f645a496fd7d77688d8370cd0fad4
cineris <cineris1104@gmail.com>
2014-11-12 21:02:20 -0500

    Removed messages from grunt build

--- web/src/main/webapp/Gruntfile.js
+++ web/src/main/webapp/Gruntfile.js
@@ -12,7 +12,6 @@ module.exports = function(grunt) {
                     'styles/*.css',
                     'styles/*.less',
                     'scripts/**/*.html',
-                    'messages/*-messages.json',
                     'images/**'
                 ],
                 dest: 'build',
